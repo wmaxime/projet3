dba2cc94c79b3e82f6b7f2ecb7c3cea2a65dfcbd
wmaxime <wmaxime@paris.com>
2023-07-02 21:20:47 +0200

    update contract

--- client/src/components/Intro/display/messageVisitor.jsx
+++ client/src/components/Intro/display/messageVisitor.jsx
@@ -20,7 +20,7 @@ function MessageVisitor() {
 
     return (
         <div>
-            <p>Bienvenur Visiteur, </p>
+            <p>Bienvenue Visiteur, </p>
             {/* <p>Si vous souhaitez voter pour la plus belle Ferrari, veuillez vous inscrire auprès de : {displayOwner}</p> */}
             <p>Si vous êtes l'heureux propriétaire d'une Ferrari (+ son NFT) et possédez de la crypto monnaie, vous êtes potentiellement éligible pour participer à ce vote en ligne.</p>
             <p>Pour vous inscrire, veuillez envoyer 0,007859 ETH (Valeur précise correspondant au code concours) à l'adresse du gestionnaire et bien sûre à partir de votre portefeuille possédant le NFT de votre Ferrari.</p>
